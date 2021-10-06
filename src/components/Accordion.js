8108da0c8518e6bf7994d417d5890bf5d1263bd5
Ganesh Reddy Chimmula <90820043+Ganesh-rc@users.noreply.github.com>
2021-10-06 11:59:37 +0530

    Update Accordion.js
    Checking if index is displaying

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,22 +1,29 @@
 import React from 'react';
 
 const Accordion = ({items}) => {
-    const renderedItems = items.map((item) => {
-        return <div key={item.title}>
-            <div className="title active">
-                <i className="dropdown icon"></i>
-                {item.title}
-            </div> 
-            <div className="content active">
-                <p>item.content</p>
-            </div>
-            
-        </div>
+    // creating a 
+    const renderedItems = items.map((item, index) => {
+        return(
+            <React.Fragment key={item.title}>
+                <div 
+                className="title active"
+                onClick={() => console.log('title clicked',index)}
+                >
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div> 
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>          
+            </React.Fragment>
+        );    
     }
     );
 
     return (
-        <div className="ui styled accordion"></div>
+        <div className="ui styled accordion">
+            {renderedItems}
+        </div>
     );
 };
 
