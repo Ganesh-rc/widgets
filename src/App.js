9c8501f4db4ccd9d97478eee2531bc4b5c09bdd8
Ganesh Reddy Chimmula <90820043+Ganesh-rc@users.noreply.github.com>
2021-10-07 17:43:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Accordion from './components/Accordion';
-import Search from './components/Search';
+// import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+
+const options = [
+    {
+        label : 'The colour red',
+        value:'red'
+    },
+    {
+        label :"the color green",
+        value:'green'
+    },
+    {
+        label :"A shade of blue",
+        value:'blue'
+    }
+];
+
 const items =[{
     title:'what is react',
     content:'React is a front end javascript framework'
@@ -16,9 +33,21 @@ const items =[{
 ];
 
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
+    
     return (
         <div>
-            <div><Search /></div>
+            <div>
+                <button onClick={() => setShowDropdown(!showDropdown)}>Toggle dropdown</button> 
+                {showDropdown ?
+                    <Dropdown 
+                    selected = {selected}
+                    onSelectedChange = {setSelected}
+                    options ={ options } 
+                /> : null
+            }
+            </div>            
         </div>
 
     );
