7e22fab2afbf278e7eda6a60ecb6347382006a3f
Ganesh Reddy Chimmula <90820043+Ganesh-rc@users.noreply.github.com>
2021-10-08 09:27:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-// import Accordion from './components/Accordion';
-// import Search from './components/Search';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const options = [
     {
@@ -32,12 +35,38 @@ const items =[{
 },
 ];
 
+
 const App = () => {
     const [selected, setSelected] = useState(options[0]);
-    const [showDropdown, setShowDropdown] = useState(true);
+    // const [showDropdown, setShowDropdown] = useState(true);
     
     return (
         <div>
+            <Header />
+            <Route path='/' >
+                <Accordion items={items}/>
+            </Route>
+            <Route path='/list' >
+                <Search />;
+            </Route>
+            <Route path='/dropdown' >
+                <Dropdown 
+                label="select a color"
+                options={options}
+                selected = {selected}
+                onSelectedChange={setSelected}
+                />;
+            </Route>
+            <Route path='/translate' >
+                <Translate />;
+            </Route>
+        </div>
+
+    );
+};
+
+export default App;
+/*
             <div>
                 <button onClick={() => setShowDropdown(!showDropdown)}>Toggle dropdown</button> 
                 {showDropdown ?
@@ -47,10 +76,5 @@ const App = () => {
                     options ={ options } 
                 /> : null
             }
-            </div>            
-        </div>
-
-    );
-};
-
-export default App;
+            </div>  
+*/ 
